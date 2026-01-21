e129b6e231f179fb0766475757d8491fba0e1618
Carsten Bauer <crstnbr@gmail.com>
2026-01-21 14:24:32 +0100

    tmp

---
+++ orga/local_install.sh
@@ -0,0 +1,14 @@
+#
+# to be executed in the HOME directory of the user accounts on the HLRS laptops
+#
+
+# install juliaup + julia (if julia doesn't already exist)
+if ! command -v julia &>/dev/null; then
+  echo "julia could not be found, installing juliaup + julia ..."
+  curl -fsSL https://install.julialang.org | sh -s -- --yes
+fi
+
+# install workshop environment (includes LIKWID)
+git clone https://github.com/carstenbauer/JuliaKIT26
+cd JuliaKIT26
+julia install.jl full
