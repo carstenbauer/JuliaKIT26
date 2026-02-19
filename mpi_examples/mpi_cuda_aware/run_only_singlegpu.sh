a22d2c2215840e1ac77683b89a8576c6938bd6ce
Carsten Bauer <crstnbr@gmail.com>
2026-02-19 21:54:15 +0100

    cuda-aware mpi tests successful

--- mpi_examples/mpi_cuda_aware/run_only_singlegpu.sh
+++ mpi_examples/mpi_cuda_aware/run_only_singlegpu.sh
@@ -1,8 +1,1 @@
-# uncomment this if you are on cluster gpu node
-ml juliahpc
-ml mpi/openmpi
-export OMPI_MCA_mpi_cuda_support=1
-export OMPI_MCA_mca_component_show_load_errors=0
-export JULIA_CUDA_MEMORY_POOL=none
-
 mpiexecjl --project -n 4 julia 1_cudampi_singlegpu.jl
